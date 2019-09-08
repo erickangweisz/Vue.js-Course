141dda2db3e8f3e44f3689bfaad61f1e40cf545e
jkang <erickangweisz@gmail.com>
2019-09-08 18:23:03 +0100

    Change HelloWord component to HomeComponent and use it in App.vue. Import and implement router.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -12,6 +13,7 @@ Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
